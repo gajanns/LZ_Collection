9862b88a23c1a0c079c631fccf6f907ce691f64e
gajanns <44852218+gajanns@users.noreply.github.com>
2024-04-14 11:27:07 +0000

    Clean up

--- include/SuffixArray.hpp
+++ include/SuffixArray.hpp
@@ -8,16 +8,21 @@
 #include "Definition.hpp"
 
 /**
- * Suffix Array by induced sorting
- * https://zork.net/~st/jottings/sais.html
- * https://www.rahmannlab.de/lehre/alsa21/02-3-sais.pdf
+ * Suffix Array by Induced Sorting
+ * Modified from Source: https://www.rahmannlab.de/lehre/alsa21/02-3-sais.pdf
 */
-
 namespace SuffixArray
 {
     enum LS_Type {S, L};
+
     std::vector<int> generate_suffix_array(const NumRange auto &p_value, size_t alphabet_size);
 
+    /**
+     * @brief Generate Suffix Array using Naive Sorting
+     * 
+     * @param p_value Data-Sequence to generate Suffix Array from
+     * @return std::vector<int> Suffix Array
+     */
     std::vector<int> generate_suffixarray_naive(const std::string &p_value) {
     
         std::vector<std::string> suffixes;
@@ -35,46 +40,13 @@ namespace SuffixArray
         return result;
     }
 
-    std::vector<SuffixArray::LS_Type> compute_types(const NumRange auto &p_value) {
-        
-        std::vector<LS_Type> result(p_value.size()+1, S);
-        
-        for(int i = p_value.size()-1; i >= 0; i--) {
-            if( i == static_cast<int>(p_value.size())-1) {
-                result[i] = L;
-                continue;
-            }
-            result[i] = (p_value[i] < p_value[i+1]) ? S : (p_value[i] > p_value[i+1]) ? L : result[i+1];
-        }
-
-        return result;
-    }
-
-    std::vector<size_t> count_cumulative_characters(const NumRange auto &p_value, size_t alphabet_size) {
-        
-        std::vector<size_t> result(alphabet_size+1, 0);
-
-        std::for_each(p_value.begin(), p_value.begin()+p_value.size(), [&result](auto c) {
-            result[c]++;
-        });
-        result[0] = 1; // Sentinel
-
-        std::inclusive_scan(result.begin(), result.end(), result.begin());
-        return result;
-    }
-
-    std::vector<size_t> find_lms_positions(std::vector<LS_Type> &p_type) {
-        
-        std::vector<size_t> result;
-        for(size_t i = 1; i < p_type.size(); i++) {
-            if(p_type[i] == S && p_type[i-1] == L) {
-                result.push_back(i);
-            }
-        }
-        return result;
-    }
-
-    void initialize_pos_from_lms(const NumRange auto &p_value, std::vector<size_t> p_bucket_sizes, 
+    /**
+     * @brief Preprocess Suffix Array using LMS Substrings
+     * 
+     * @param p_sa Suffix Array
+     * @return std::vector<int> Initial Suffix Array
+     */
+    void estimate_sa(const NumRange auto &p_value, std::vector<size_t> p_bucket_sizes, 
     std::vector<size_t> const &p_lms_positions, std::vector<int> &p_sa) {
         
         std::fill(p_sa.begin(), p_sa.end(), -1);
@@ -88,6 +60,14 @@ namespace SuffixArray
         }
     }
 
+    /**
+     * @brief Fill L-Type Suffixes into Buckets of Suffix Array
+     * 
+     * @param p_value Data-Sequence
+     * @param p_bucket_sizes Bucket Sizes defined by first character
+     * @param p_types Type-Sequence of Data-Sequence
+     * @param p_sa Modified Suffix Array (Output)
+     */
     void induce_sort_L(const NumRange auto &p_value, std::vector<size_t> p_bucket_sizes, std::vector<LS_Type> const &p_types, std::vector<int> &p_sa) {
         
         for( auto p : p_sa) {
@@ -97,6 +77,14 @@ namespace SuffixArray
         }
     }
 
+    /**
+     * @brief Fill S-Type Suffixes into Buckets of Suffix Array
+     * 
+     * @param p_value Data-Sequence
+     * @param p_bucket_sizes Bucket Sizes defined by first character
+     * @param p_types Type-Sequence of Data-Sequence
+     * @param p_sa Modified Suffix Array (Output)
+     */
     void induce_sort_S(const NumRange auto &p_value, std::vector<size_t> p_bucket_sizes, std::vector<LS_Type> const &p_types, std::vector<int> &p_sa) {
         
         for( auto p : p_sa | std::ranges::views::reverse) {
@@ -109,11 +97,21 @@ namespace SuffixArray
     void phase2(const NumRange auto &p_value, std::vector<size_t> &p_bucket_sizes, 
     std::vector<LS_Type> const &p_types, std::vector<size_t> const &p_lms_positions, std::vector<int> &p_sa) {
         
-        initialize_pos_from_lms(p_value, p_bucket_sizes, p_lms_positions, p_sa);
+        estimate_sa(p_value, p_bucket_sizes, p_lms_positions, p_sa);
         induce_sort_L(p_value, p_bucket_sizes, p_types, p_sa);
         induce_sort_S(p_value, p_bucket_sizes, p_types, p_sa);
     }
 
+    /**
+     * @brief Compare two LMS Substrings
+     * 
+     * @param p_value Data-Sequence
+     * @param p_types Type-Sequence
+     * @param p_pos1 Offset of first LMS Substring
+     * @param p_pos2 Offset of second LMS Substring
+     * @return true LMS Substrings are unequal
+     * @return false LM Substrings are equal
+     */
     bool lms_substrings_unequal(const NumRange auto &p_value, std::vector<LS_Type> const &p_types, size_t p_pos1, size_t p_pos2) {
         
         bool is_lms1 = false, is_lms2 = false;
@@ -136,6 +134,15 @@ namespace SuffixArray
         }
     }
 
+    /**
+     * @brief Reduce Data-Sequence into Sequence of LMS Substrings
+     * 
+     * @param p_value Data-Sequence
+     * @param p_types Type-Sequence
+     * @param p_sa Current Suffix Array of Data-Sequence
+     * @param p_lms_positions Sequence of LMS Positions
+     * @return std::tuple<std::vector<int>, size_t, std::vector<int>> {Reduced Data-Sequence, Reduced Alphabet Size, Position Map}
+     */
     std::tuple<std::vector<int>, size_t, std::vector<int>> reduce_text(const NumRange auto &p_value, std::vector<LS_Type> const &p_types, std::vector<int> &p_sa, std::vector<size_t> &p_lms_positions) {
         std::vector<int> lms_names(p_sa.size()-1, -1);
         size_t last_lms = lms_names.size();
@@ -188,18 +195,58 @@ namespace SuffixArray
         }
     }
 
+    /**
+     * @brief Generate Suffix Array using Induced Sorting
+     * 
+     * @param p_value Data-Sequence to generate Suffix Array from(without Sentinel)
+     * @param alphabet_size Size of Alphabet(without Sentinel)
+     * @return std::vector<int> Suffix Array
+     */
     std::vector<int> generate_suffix_array(const NumRange auto &p_value, size_t alphabet_size) {
         
         std::vector<int> result(p_value.size()+1, -1);
-        std::vector<LS_Type> types = compute_types(p_value);
-        std::vector<size_t> bucket_sizes = count_cumulative_characters(p_value, alphabet_size);
-        std::vector<size_t> lms_positions = find_lms_positions(types);
+        std::vector<LS_Type> types(p_value.size()+1, S);
+        std::vector<size_t> bucket_sizes(alphabet_size+1, 0);
+        std::vector<size_t> lms_positions;
+
+        /* #region Compute Types (Sentinel not in p_value) */
+        for(int i = p_value.size()-1; i >= 0; i--) {
+            if( i == static_cast<int>(p_value.size())-1) {
+                types[i] = L;
+                continue;
+            }
+            types[i] = (p_value[i] < p_value[i+1]) ? S : (p_value[i] > p_value[i+1]) ? L : types[i+1];
+        }
+        /* #endregion */
+
+        /* #region Compute Bucket-Sizes (Sentinel not in Alphabet) */
+        std::for_each(p_value.begin(), p_value.begin()+p_value.size(), [&bucket_sizes](auto c) {
+            bucket_sizes[c]++;
+        });
+        bucket_sizes[0] = 1; // Sentinel
+
+        std::inclusive_scan(bucket_sizes.begin(), bucket_sizes.end(), bucket_sizes.begin());
+        /* #endregion */
+
+        /* #region Compute LMS Positions */
+        for(size_t i = 1; i < types.size(); i++) {
+            if(types[i] == S && types[i-1] == L) {
+                lms_positions.push_back(i);
+            }
+        }
+       /* #endregion */
 
         phase1(p_value, bucket_sizes, types, lms_positions, result);
         phase2(p_value, bucket_sizes, types, lms_positions, result);
         return result;
     }
 
+    /**
+     * @brief Generate Inverse Suffix Array
+     * 
+     * @param p_sa Suffix Array
+     * @return std::vector<int> Inverse Suffix Array
+     */
     std::vector<int> invert_suffix_array(const std::vector<int> &p_sa) {
         
         std::vector<int> result(p_sa.size(), -1);
