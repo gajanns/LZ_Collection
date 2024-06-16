84a077ac7e9a67a075cdbdcb917269ce89968338
Gajann <gajann@live.de>
2024-06-16 13:50:00 +0000

    Further work on Plotting

--- report/plots_progressive.py
+++ report/plots_progressive.py
@@ -4,33 +4,39 @@ import csv
 import os
 
 
-col_input_size, col_output_size, col_n_factors, col_comp_time, col_mem_usage = 0, 1, 2, 3, 4
+col_input_size, col_output_size, col_n_factors, col_comp_time, col_mem_usage, col_init_time, col_match_time, col_io_time = 0, 1, 2, 3, 4, 5, 6, 7
+
 
 
 def read_data():
-    report_data = np.empty((0, 5))
-    input_rows = {}
-    algorithm_rows = {}
+    
+    report_csv = csv.DictReader(open("report_progressive.csv", "r"))
+    report_data = np.empty([0,len(report_csv.fieldnames)-2], int)
+    filename = ""
+    algorithm = ""
 
-    report_csv = csv.DictReader(open("report.csv", "r"))
     for id,row in enumerate(report_csv):
-        if row["input"] not in input_rows:
-            input_rows[row["input"]] = []
-        input_rows[row["input"]].append(id)
-        if row["algorithm"] not in algorithm_rows:
-            algorithm_rows[row["algorithm"]] = []
-        algorithm_rows[row["algorithm"]].append(id)
-        report_data = np.append(report_data, np.array([[int(row["in_size"]), int(row["out_size"]), int(row["n_factors"]), 
-                                            int(row["comp_time"]), int(row["mem_usage"])]]), axis=0)
+        if id == 0:
+            filename = row["input"]
+            algorithm = row["algorithm"]
+        else:
+            if filename != row["input"] or algorithm != row["algorithm"]:
+                print("Error: Inconsistent data in report.csv")
+                return
+        report_row = list(map(lambda x: int(row[x]), [name for name in report_csv.fieldnames if name != "input" and name != "algorithm"]))
+        report_data = np.append(report_data, np.array([report_row]), axis=0)
 
-    return report_data, input_rows, algorithm_rows
+    return report_data, filename, algorithm
 
 def main():
-    if not os.path.exists('report.csv'):
+    if not os.path.exists('report_progressive.csv'):
         print("No report.csv found.")
         return
     if not os.path.exists('plots'):
         os.mkdir('plots')
     
-    report_data, input_rows, algorithm_rows = read_data()
-    x_input, x_algo = input_rows.keys(), algorithm_rows.keys()
+    report_data, filename, algorithm = read_data()
+    
+
+if __name__ == "__main__":
+    main()
